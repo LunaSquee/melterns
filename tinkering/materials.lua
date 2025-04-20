31b7b63c10d8861a5d2378deb29d3fbfce73d08d
Evert Prants <evert@lunasqu.ee>
2025-04-20 12:34:37 +0300

    Tweak groups a bit more

--- tinkering/materials.lua
+++ tinkering/materials.lua
@@ -50,10 +50,10 @@ local modifiers = {
 		}
 	},
 	steel = {
-		cracky = {times={[1]=4.00, [2]=1.60, [3]=0.80, [4]=0.40}, uses=20, maxlevel=4},
+		cracky = {times={[1]=4.00, [2]=1.60, [3]=0.80, [4]=0.70}, uses=20, maxlevel=4},
 		crumbly = {times={[1]=1.50, [2]=0.90, [3]=0.40, [4]=0.30}, uses=30, maxlevel=4},
 		snappy = {times={[1]=2.50, [2]=1.20, [3]=0.35, [4]=0.30}, uses=30, maxlevel=4},
-		choppy = {times={[1]=2.50, [2]=1.40, [3]=1.00, [4]=0.80}, uses=20, maxlevel=4},
+		choppy = {times={[1]=2.50, [2]=1.40, [3]=1.00, [4]=0.90}, uses=20, maxlevel=4},
 		damagegroups = {fleshy = 6},
 		explody = nil,
 
@@ -65,10 +65,10 @@ local modifiers = {
 		}
 	},
 	copper = {
-		cracky = {times={[1]=3.80, [2]=1.50, [3]=0.85, [4]=0.60}, uses=20, maxlevel=4},
-		crumbly = {times={[1]=1.30, [2]=0.80, [3]=0.45, [4]=0.30}, uses=30, maxlevel=4},
-		snappy = {times={[1]=2.30, [2]=1.10, [3]=0.35, [4]=0.30}, uses=30, maxlevel=4},
-		choppy = {times={[1]=2.30, [2]=1.30, [3]=0.95, [4]=0.60}, uses=20, maxlevel=4},
+		cracky = {times={[1]=3.80, [2]=1.50, [3]=0.85, [4]=0.75}, uses=20, maxlevel=4},
+		crumbly = {times={[1]=1.30, [2]=0.80, [3]=0.45, [4]=0.35}, uses=30, maxlevel=4},
+		snappy = {times={[1]=2.30, [2]=1.10, [3]=0.35, [4]=0.35}, uses=30, maxlevel=4},
+		choppy = {times={[1]=2.30, [2]=1.30, [3]=0.95, [4]=0.95}, uses=20, maxlevel=4},
 		damagegroups = {fleshy = 5},
 		explody = nil,
 
@@ -79,10 +79,10 @@ local modifiers = {
 		}
 	},
 	tin = {
-		cracky = {times={[1]=3.00, [2]=1.50, [3]=0.60}, uses=20, maxlevel=2},
-		crumbly = {times={[1]=2.80, [2]=1.40, [3]=0.60}, uses=30, maxlevel=2},
-		snappy = {times={[1]=2.50, [2]=1.00, [3]=0.50}, uses=30, maxlevel=2},
-		choppy = {times={[1]=2.60, [2]=1.20, [3]=0.80}, uses=20, maxlevel=2},
+		cracky = {times={[1]=3.00, [2]=1.50, [3]=0.80}, uses=20, maxlevel=2},
+		crumbly = {times={[1]=2.80, [2]=1.40, [3]=0.80}, uses=30, maxlevel=2},
+		snappy = {times={[1]=2.50, [2]=1.00, [3]=0.80}, uses=30, maxlevel=2},
+		choppy = {times={[1]=2.60, [2]=1.20, [3]=1.00}, uses=20, maxlevel=2},
 		damagegroups = {fleshy = 5},
 		explody = nil,
 
@@ -95,7 +95,7 @@ local modifiers = {
 	bronze = {
 		cracky = {times={[1]=3.00, [2]=1.60, [3]=0.80}, uses=30, maxlevel=3},
 		crumbly = {times={[1]=2.50, [2]=0.90, [3]=0.40}, uses=40, maxlevel=3},
-		snappy = {times={[1]=2.50, [2]=1.20, [3]=0.35}, uses=40, maxlevel=3},
+		snappy = {times={[1]=2.50, [2]=1.20, [3]=0.80}, uses=40, maxlevel=3},
 		choppy = {times={[1]=2.50, [2]=1.40, [3]=1.00}, uses=30, maxlevel=3},
 		damagegroups = {fleshy = 6},
 		explody = nil,
@@ -149,10 +149,10 @@ local modifiers = {
 		}
 	},
 	lead = {
-		cracky = {times={[1]=3.70, [2]=1.30, [3]=0.60, [4]=0.40}, uses=20, maxlevel=4},
-		crumbly = {times={[1]=2.40, [2]=0.60, [3]=0.60, [4]=0.40}, uses=30, maxlevel=4},
-		snappy = {times={[1]=2.20, [2]=1.00, [3]=0.60, [4]=0.40}, uses=30, maxlevel=4},
-		choppy = {times={[1]=2.20, [2]=1.20, [3]=0.60, [4]=0.40}, uses=20, maxlevel=4},
+		cracky = {times={[1]=3.70, [2]=1.30, [3]=0.60, [4]=0.50}, uses=20, maxlevel=4},
+		crumbly = {times={[1]=2.40, [2]=0.60, [3]=0.60, [4]=0.50}, uses=30, maxlevel=4},
+		snappy = {times={[1]=2.20, [2]=1.00, [3]=0.60, [4]=0.50}, uses=30, maxlevel=4},
+		choppy = {times={[1]=2.20, [2]=1.20, [3]=0.60, [4]=0.50}, uses=20, maxlevel=4},
 		damagegroups = {fleshy = 7},
 		explody = nil,
 
@@ -164,10 +164,10 @@ local modifiers = {
 	},
 	chromium = {
 		-- TODO: tweak
-		cracky = {times={[1]=3.70, [2]=1.30, [3]=0.60, [4]=0.40}, uses=20, maxlevel=4},
+		cracky = {times={[1]=3.70, [2]=1.30, [3]=0.60, [4]=0.50}, uses=20, maxlevel=4},
 		crumbly = {times={[1]=2.40, [2]=1.20, [3]=0.60, [4]=0.40}, uses=30, maxlevel=4},
-		snappy = {times={[1]=2.20, [2]=1.20, [3]=0.60, [4]=0.40}, uses=30, maxlevel=4},
-		choppy = {times={[1]=2.20, [2]=1.20, [3]=0.60, [4]=0.40}, uses=20, maxlevel=4},
+		snappy = {times={[1]=2.20, [2]=1.20, [3]=0.60, [4]=0.50}, uses=30, maxlevel=4},
+		choppy = {times={[1]=2.20, [2]=1.20, [3]=0.60, [4]=0.50}, uses=20, maxlevel=4},
 		damagegroups = {fleshy = 5},
 		explody = nil,
 
@@ -208,7 +208,7 @@ local modifiers = {
 		}
 	},
 	mithril = {
-		cracky = {times = {[1] = 3.00, [2] = 1.60, [3] = 0.60, [4] = 0.40}, uses = 40, maxlevel = 4},
+		cracky = {times = {[1] = 3.00, [2] = 1.60, [3] = 0.60, [4] = 0.50}, uses = 40, maxlevel = 4},
 		crumbly = {times = {[1] = 2.80, [2] = 1.50, [3] = 1.00, [4] = 0.60}, uses = 40, maxlevel = 4},
 		snappy = {times = {[1] = 3.00, [2] = 2.60, [3] = 1.25, [4] = 0.60}, uses = 40, maxlevel = 4},
 		choppy = {times = {[1] = 2.00, [2] = 1.45, [3] = 0.60, [4] = 0.45}, uses = 40, maxlevel = 4},
