ed3f41f9ec98704f4103fd013290ea13007c3de0
CodingMantisUA <coding.mantis.ua@pm.me>
2026-02-12 00:31:19 +0200

    Controller activates on forming multiblock

--- multifurnace/api.lua
+++ multifurnace/api.lua
@@ -241,6 +241,14 @@ end
 -- Furnace lifecycle --
 -----------------------
 
+local function swap_node(pos, node, name)
+    if node.name == name then
+        return
+    end
+    node.name = name
+    core.swap_node(pos, node)
+end
+
 function multifurnace.api.check_controller(pos)
     local check_serial = math.random(10000, 99999)
     local node = core.get_node_or_nil(pos)
@@ -261,6 +269,7 @@ function multifurnace.api.check_controller(pos)
         multifurnace.fluid_entity.remove(pos)
         notify_ports_removal(pos)
         update_timer(pos)
+        swap_node(pos, node, "multifurnace:controller")
         return
     end
 
@@ -283,6 +292,8 @@ function multifurnace.api.check_controller(pos)
     end
 
     local volume = calculate_volume(dimensions)
+    swap_node(pos, node, "multifurnace:controller_active")
+    def = core.registered_nodes["multifurnace:controller_active"]
     multifurnace.loaded_controllers[key] = {
         controller = pos,
         serial = check_serial,
