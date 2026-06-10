951ac3134af90633bcf496688a566a23ba789ba1
CodingMantis <coding.mantis.ua@pm.me>
2026-06-11 01:17:13 +0300

    binding texture support

--- tinkering/util.lua
+++ tinkering/util.lua
@@ -3,9 +3,11 @@ function tinkering.color_filter(texture, color)
 	return texture.."^[multiply:"..color
 end
 
-function tinkering.combine_textures(tex1, tex2, color1, color2, offset, dp)
+function tinkering.combine_textures(tex1, tex2, color1, color2, offset, tex3, color3)
 	local main_tex = tex1 .."\\^[multiply\\:".. color1
 	local rod_tex  = tex2 .."\\^[multiply\\:".. color2
+	local binding_tex = tex3 .."\\^[multiply\\:".. color3
 
-	return "[combine:16x16:"..offset.."="..main_tex..":0,0="..rod_tex
+	return "[combine:16x16:"..offset.."="..main_tex..
+		":0,0="..binding_tex..":0,0="..rod_tex
 end
